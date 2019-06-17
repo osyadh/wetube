dc3644725e68ea4b216fee0b6dd6c9b61d6005c8
Sung Dongho <fencer0565@gmail.com>
2019-06-17 23:18:28 +0900

    ~#5

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WeTube
 
 Cloning Youtube with VanillaJS and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ routers/globalRouter.js
@@ -0,0 +1,25 @@
+import express from "express";
+import routes from "../routes";
+import { home, search } from "../controllers/videoController";
+import {
+  login,
+  logout,
+  getJoin,
+  postJoin,
+  postLogin,
+  getLogin
+} from "../controllers/userController";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, logout);
+
+export default globalRouter;
