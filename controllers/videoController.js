d85ab540e2ec0047756a46db83457fa4f277b5bb
Sung Dongho <fencer0565@gmail.com>
2019-07-22 13:46:26 +0900

    ~#7

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -35,8 +35,11 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -45,7 +48,8 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
@@ -58,6 +62,9 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
+    if (String(video.creator) !== req.user.id) {
+      throw Error();
+    }
     res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
   } catch (error) {
     res.redirect(routes.home);
@@ -81,7 +88,11 @@ export const deleteVideo = async (req, res) => {
   const {
     params: { id }
   } = req;
+  const video = await Video.findById(id);
   try {
+    if (String(video.creator) !== req.user.id) {
+      throw Error();
+    }
     await Video.findOneAndRemove({ _id: id });
   } catch (error) {
     console.log(error);
--- models/Video.js
+++ models/Video.js
@@ -23,7 +23,8 @@ const VideoSchema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: "Comment"
     }
-  ]
+  ],
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
 const model = mongoose.model("Video", VideoSchema);
